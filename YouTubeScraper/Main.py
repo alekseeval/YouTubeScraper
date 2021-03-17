898f9e85f5119e23092487b21142a797a3fe9a7a
alekseeval <alekseeval2000@mail.ru>
2021-03-17 18:03:01 +0800

    Добавлена предобработка данных (Длительность видео)

--- YouTubeScraper/Main.py
+++ YouTubeScraper/Main.py
@@ -3,6 +3,7 @@ from YouTubeScraperApi import YouTubeScrapper
 import pandas as pd
 import numpy as np
 from pprint import pprint
+import isodate
 
 
 # ------------------------------------------------------------------------------------------------
@@ -12,16 +13,19 @@ def deleteUselessColumns(data):
     del data['video_id']
     del data['playlist_id']
 
+def formatDuration(data):
+    data['duration'] = data['duration'].map(lambda date: isodate.parse_duration(date).total_seconds())
 
 # ------------------------------------------------------------------------------------------------
 def main():
     # Получение данных о видео с канала
-    ys = YouTubeScrapper("UCdKuE7a2QZeHPhDntXVZ91w")
+    ys = YouTubeScrapper("UC8M5YVWQan_3Elm-URehz9w")
     data = ys.getAllVideosInfo()
     channelTitle = ys.getChannelTitle()
 
     # Предобработка данных
     deleteUselessColumns(data)
+    formatDuration(data)
 
     # Запись данных в CSV файл
     data.to_csv('result/' + channelTitle + '.csv',
