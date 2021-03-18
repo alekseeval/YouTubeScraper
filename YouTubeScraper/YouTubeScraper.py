57d149f4affebbfad4e4ee5a501491b43789e444
HyperMarshmallow <alekseeval2000@mail.ru>
2021-03-19 03:15:00 +0800

    Реализовано получение списка всех плейлистов канала

--- YouTubeScraper/YouTubeScraper.py
+++ YouTubeScraper/YouTubeScraper.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+from time import sleep
 from pprint import pprint
 
 
@@ -6,20 +11,53 @@ class YouTubeScraper:
 
     def __init__(self, channel):
         self.driver = webdriver.Chrome('chromedriver.exe')
+        self.driver.maximize_window()
         self.channel = channel
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # Возвращает все ссылки на плейлисты канала self.channel
+    # ------------------------------------------------------------------------------------------------------------------
     def get_all_playlists_links(self):
-
-        return []
-
-    def get_all_videos_info(self):
         self.driver.get("https://www.youtube.com/channel/" + self.channel + "/playlists?view=1&sort=dd&shelf_id=0")
+        self.__scroll_page_to_bottom()
+        a_tags = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_all_elements_located((By.LINK_TEXT, 'ПОСМОТРЕТЬ ВЕСЬ ПЛЕЙЛИСТ'))
+        )
+        links = []
+        for a in a_tags:
+            links.append(a.get_attribute('href'))
+        return links
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Метод скроллит текущую страницу вниз до конца
+    # ------------------------------------------------------------------------------------------------------------------
+    def __scroll_page_to_bottom(self):
+        load_pause = 1  # Пауза в сек для подгрузки данных
+        box = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="page-manager"]/ytd-browse/ytd-two-column-browse-results-renderer')
+            )
+        )
+        last_height = box.size['height']
+        while True:
+            self.driver.execute_script("window.scrollBy(0, " + str(8000) + ");")
+            sleep(load_pause)
+            new_height = box.size['height']
+            if new_height == last_height:
+                break
+            else:
+                last_height = new_height
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Возвращает DataFrame со всей нужной инофрмацией о видео на канале
+    # ------------------------------------------------------------------------------------------------------------------
+    def get_all_videos_info(self):
         playlists_links = self.get_all_playlists_links()
 
         pprint(playlists_links)
         self.driver.close()
 
-
+# ----------------------------------------------------------------------------------------------------------------------
 def main():
     ys = YouTubeScraper('UCdKuE7a2QZeHPhDntXVZ91w')
     ys.get_all_videos_info()
